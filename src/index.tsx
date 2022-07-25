6228bfd25a0f7558b600cbfccc897b9a2b6bdb77
Karl Cereno <kagc.dev@gmail.com>
2022-07-25 12:56:55 -0400

    Wrap index.tsx with DatabaseProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import DatabaseProvider from "./store/DatabaseProvider";
 import ModalProvider from "./store/ModalProvider";
 import UserProvider from "./store/UserProvider";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <ModalProvider>
-          <App />
-        </ModalProvider>
-      </UserProvider>
+      <DatabaseProvider>
+        <UserProvider>
+          <ModalProvider>
+            <App />
+          </ModalProvider>
+        </UserProvider>
+      </DatabaseProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
