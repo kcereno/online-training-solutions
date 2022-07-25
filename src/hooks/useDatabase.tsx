29d584e7cf1fd466d851758499cf132b09793aa6
Karl Cereno <kagc.dev@gmail.com>
2022-07-25 13:01:59 -0400

    Update useDatabase to use database state

--- src/hooks/useDatabase.tsx
+++ src/hooks/useDatabase.tsx
@@ -1,26 +1,34 @@
 // Simulates interacting with database
-import { useCallback } from "react";
-import { DUMMY_DATA, deleteUser, addUser, updateUser } from "../data/DUMMY_DB";
+// Performs CRUD operations on database itself
+
+import { useCallback, useContext } from "react";
+import { deleteUser, addUser, updateUser } from "../data/DUMMY_DB";
 import { User } from "../data/types";
+import DatabaseContext from "../store/database-context";
 
 const useDatabase = () => {
+  const { database, updateDatabase } = useContext(DatabaseContext);
+
   const fetchUser = (userId: string) =>
-    DUMMY_DATA.find((user) => user.info.id === userId);
+    database.find((user) => user.info.id === userId);
 
   // Fetches users by id present in passed in array
-  const fetchUsers = useCallback((users: string[]) => {
-    let results: User[] = [];
+  const fetchUsers = useCallback(
+    (users: string[]) => {
+      let results: User[] = [];
 
-    users.forEach((userId) => {
-      const foundUser = DUMMY_DATA.find((user) => user.info.id === userId);
-      results.push(foundUser!);
-    });
-    return results;
-  }, []);
+      users.forEach((userId) => {
+        const foundUser = database.find((user) => user.info.id === userId);
+        results.push(foundUser!);
+      });
+      return results;
+    },
+    [database]
+  );
 
   // Validates user email and password
   const validateUser = (email: string, password: string) =>
-    DUMMY_DATA.find(
+    database.find(
       (user) => user.info.email === email && user.info.password === password
     );
 
@@ -31,7 +39,7 @@ const useDatabase = () => {
     fetchUsers,
     fetchUser,
     updateDBUser: updateUser,
-    userDB: DUMMY_DATA,
+    userDB: database,
   };
 };
 
