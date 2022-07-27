dd42c44a8a639c4efac655bc5ccaa4fa692a1c3b
Karl Cereno <kagc.dev@gmail.com>
2022-07-27 13:03:54 -0400

    Make rough data presentation

--- src/pages/ClientProfilePage/ClientProfilePage.tsx
+++ src/pages/ClientProfilePage/ClientProfilePage.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
-import { Client } from "../../data/interfaces";
-
 import "./ClientProfilePage.css";
 import { useTrainerActions } from "../../hooks/useTrainerActions";
 
@@ -20,61 +18,40 @@ const ClientProfilePage = () => {
       <Card style={cardStyle}>
         <h1>{client?.info.firstName}'s' profile page</h1>
       </Card>
+
       <Card style={cardStyle}>
         <h1>{client?.info.firstName}'s Program</h1>
-        {/* {client!.trainingPlan.assignedExercises.map((exercise: any) => {
+        {client!.trainingPlan.assignedExercises.map((exercise: any) => {
           return (
             <p>{`Exercise: ${exercise.name} Target Weight: ${exercise.weight} Target Reps:${exercise.reps} Target Sets: ${exercise.sets}`}</p>
           );
-        })} */}
+        })}
       </Card>
 
-      {/* <Row>
-        <Col xs={3} style={{ background: "#121212", borderRadius: "25px" }}>
-          <div className="profile-head text-center my-3">
-            <img
-              src={client?.info.profilePicture}
-              className="profile-picture rounded "
-              alt=""
-            />
-            <h4 className="lead mt-3">{`${client?.info.firstName} ${client?.info.lastName}`}</h4>
-          </div>
-          <hr style={{ width: "80%", margin: "auto" }} />
-          <Container>
-            <div className="client-info mt-3">
-              <Row>
-                <Col xs={4}>
-                  <p>
-                    <strong>Email</strong>
-                  </p>
-                  <p>
-                    <strong>Birthday</strong>
-                  </p>
-                </Col>
-                <Col>
-                  <p>{client?.info.email}</p>
-                  <p>May 18, 1989</p>
-                </Col>
-              </Row>
+      <Card style={cardStyle}>
+        <h1>{client?.info.firstName}'s Log</h1>
+        {client.trainingPlan.log.map((logEntry) => {
+          return (
+            <div>
+              <p>Date: {logEntry.date.toDateString()}</p>
+              {logEntry.data.map((exercise) => {
+                return (
+                  <div>
+                    <p>Exercise: {exercise.exercise}</p>
+                    {exercise.data.map((set) => {
+                      return (
+                        <p>
+                          Set: {set.set} Weight: {set.weight} Reps: {set.reps}
+                        </p>
+                      );
+                    })}
+                  </div>
+                );
+              })}
             </div>
-            <hr style={{ width: "100%", margin: "auto" }} />
-            <div className="client-training-plan mt-3">
-              <Row>
-                <Col xs={4}>
-                  <p>Trainer:</p>
-                  <p>Goal</p>
-                </Col>
-                <Col>
-                  <p>Karl C</p>
-                  <p>{client?.trainingPlan.goal}</p>
-                </Col>
-              </Row>
-            </div>
-          </Container>
-        </Col>
-
-        <Col>ADD PROGRAMS HERE</Col>
-      </Row> */}
+          );
+        })}
+      </Card>
     </Container>
   );
 };
