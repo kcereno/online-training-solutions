ad145201f52a40b71ae569b7c791a94fa31374ff
Karl Cereno <kagc.dev@gmail.com>
2022-08-27 13:22:37 -0400

    Cleanup ClientDashboard

--- src/pages/ClientDashboard/ClientDashboard.tsx
+++ src/pages/ClientDashboard/ClientDashboard.tsx
@@ -1,9 +1,7 @@
 import { Accordion, Container, Card } from "react-bootstrap";
-import { AccordianItem } from "../../components/Accordian/AccordianItem";
 import { Client } from "../../data/interfaces";
 import { useAccordionButton } from "react-bootstrap/AccordionButton";
-import { isToday, convertDate, today } from "../../data/functions";
-import useClientActions from "../../hooks/useClientActions";
+import { today } from "../../data/functions";
 import ExerciseLogEntryForm from "../../components/ExerciseLogEntryForm/ExerciseLogEntryForm";
 import { LogEntry } from "../../data/interfaces";
 
