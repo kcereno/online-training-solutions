1db0cdeb7676c4ab94b8ac8f4f850826fa610896
Karl Cereno <karlcereno@gmail.com>
2022-08-29 20:05:48 +0000

    Refactor NavBar.tsx

--- src/UI/NavBar/NavBar.tsx
+++ src/UI/NavBar/NavBar.tsx
@@ -3,18 +3,18 @@ import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import NavLinks from "./NavLinks/NavLinks";
 
-const NavBar = () => {
-  return (
-    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-      <Container fluid>
-        <Navbar.Brand href="/">
-          <FontAwesomeIcon icon={faDumbbell} className="px-2" />
-          Online Training Solutions
-        </Navbar.Brand>
-        <NavLinks />
-      </Container>
-    </Navbar>
-  );
-};
+const NavBar = () =>
+(
+  <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+    <Container fluid>
+      <Navbar.Brand href="/">
+        <FontAwesomeIcon icon={faDumbbell} className="px-2" />
+        Online Training Solutions
+      </Navbar.Brand>
+      <NavLinks />
+    </Container>
+  </Navbar>
+);
+;
 
 export default NavBar;
