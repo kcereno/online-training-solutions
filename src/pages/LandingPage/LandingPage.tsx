11e8e8f0329551f9d5f38df907eb65656627fa49
Karl Cereno <karlcereno@gmail.com>
2022-08-30 02:07:40 +0000

    Clean LandingPage.tsx

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,14 +1,9 @@
 import { Fragment } from "react";
 import CustomerReviews from "../../sections/CustomerReviewsSection/CustomerReviews";
-
 import HeroSection from "../../sections/HeroSection/HeroSection";
 import "./LandingPage.css";
 
-export default function LandingPage() {
-  return (
-    <>
-      <HeroSection />
-      <CustomerReviews />
-    </>
-  );
-}
+const LandingPage = () => <>
+  <HeroSection />
+  <CustomerReviews />
+</>
