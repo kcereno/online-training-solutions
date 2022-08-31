a3289fef6d4733951f4c42fb0bc98622e408cc35
Karl Cereno <karlcereno@gmail.com>
2022-08-31 20:20:58 +0000

    Progress on logic

--- src/hooks/useClientActions.tsx
+++ src/hooks/useClientActions.tsx
@@ -1,9 +1,10 @@
 import { useContext } from "react";
-import { convertDate } from "../data/functions";
+import { convertDate, today } from "../data/functions";
 import { Client, LogData, LogEntry, Set } from "../data/interfaces";
 import { UserType } from "../data/types";
 import DatabaseContext from "../store/Database/database-context";
 import UserContext from "../store/User/user-context";
+import { isToday } from '../data/functions'
 
 const useClientActions = () => {
   const { activeUser } = useContext(UserContext);
@@ -22,78 +23,23 @@ const useClientActions = () => {
     weight: number,
     reps: number
   ): void => {
-    //check if there a log entry for today
-    //fetchUser
-    //see if user has log entry for today
 
-    //if yes, add set to log entry
+    const newSet: Set = { weight: +weight, reps: +reps }
+    const fetchedClient = (database.find((user: UserType) => user.info.id === activeUser?.info.id)) as Client
 
-    //if no, create new log entry and add set to it
+    const todaysLog = (activeUser as Client).trainingPlan.log.find((logEntry: LogEntry) => isToday(logEntry.date))
+    console.log('todaysLog: ', todaysLog)
 
-    const newSet: Set = { weight: +weight, reps: +reps };
+    if (todaysLog) {
+      const targetExerciseIndex = todaysLog.data.findIndex((logData: LogData) => logData.exercise === exercise)
 
-    const today = convertDate(new Date());
+      console.log(targetExerciseIndex)
 
-    const client = database.find(
-      (user: UserType) => user.info.id === activeUser?.info.id
-    ) as Client;
-    console.log("useClientActions ~ client", client);
+      const updatedSets = [...todaysLog.data[targetExerciseIndex].sets, newSet]
+      console.log('updatedSets', updatedSets)
 
-    const logs = client.trainingPlan.log;
-
-    const todaysLogs = logs.find(
-      (logEntry: LogEntry) => logEntry.date.getTime() === today.getTime()
-    );
-    console.log("useClientActions ~ todaysLogs", todaysLogs);
-
-    if (todaysLogs) {
-      const exerciseIndex = todaysLogs.data.findIndex(
-        (logData: LogData) => logData.exercise === exercise
-      );
-
-      const todayIndex = logs.findIndex(
-        (logEntry: LogEntry) => logEntry.date.getTime() === today.getTime()
-      );
-      // const updatedExerciseEntry = {
-      //   ...todaysLogs.data[exerciseIndex],
-      //   sets: [...todaysLogs.data[exerciseIndex].sets, newSet],
-      // };
-      // const updatedLogData = [...todaysLogs.data];
-      // updatedLogData[exerciseIndex] = updatedExerciseEntry;
-      // const updatedLog = { ...todaysLogs, data: updatedLogData };
-      // console.log("useClientActions ~ updatedLog", updatedLog);
-      // const updatedClient = {
-      //   ...client,
-      //   trainingPlan: {
-      //     ...client.trainingPlan,
-      //     log: [...client.trainingPlan.log, updatedLog],
-      //   },
-      // };
-      // console.log("useClientActions ~ updatedClient", updatedClient);
-
-      // CURRENT ISSUE: ADDS DUPLICATE EXERCISE ETNRY TO DATA
-
-      const updatedLog: LogEntry[] = [...client.trainingPlan.log];
-      updatedLog[todayIndex] = {
-        date: today,
-        data: [
-          ...updatedLog[todayIndex].data,
-          {
-            exercise,
-            sets: [...updatedLog[todayIndex].data[exerciseIndex].sets, newSet],
-          },
-        ],
-      };
-      console.log("useClientActions ~ updatedLog", updatedLog);
-
-      const updatedClient = {
-        ...client,
-        trainingPlan: {
-          ...client.trainingPlan,
-          log: updatedLog,
-        },
-      };
-      console.log("useClientActions ~ updatedClient", updatedClient);
+      const updatedClient = { ...fetchedClient, trainingPlan: { ...fetchedClient.trainingPlan, log: [...fetchedClient.trainingPlan.log, { date: today, data: {exercise, sets: updatedSets} }] } }
+      console.log(updatedClient)
     }
   };
 
