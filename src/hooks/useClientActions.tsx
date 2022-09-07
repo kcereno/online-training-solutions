3bfc8f3483acd5fd568733d7bffb165a1988e8f2
Karl Cereno <kagc.dev@gmail.com>
2022-09-07 11:36:24 -0400

    Work on add set logic

--- src/hooks/useClientActions.tsx
+++ src/hooks/useClientActions.tsx
@@ -20,7 +20,7 @@ const useClientActions = () => {
         historyEntry.date.getTime() === today.getTime()
     );
 
-  const fetchTodaysWorkoutEntries = (date: Date) => { };
+  const fetchTodaysWorkoutEntries = (date: Date) => {};
 
   //  SET FUNCTIONS
   const addSetToLog = (
@@ -28,38 +28,77 @@ const useClientActions = () => {
     weight: number,
     reps: number
   ): void => {
+    const newSet: Set = { weight: +weight, reps: +reps };
 
-    const newSet: Set = { weight: +weight, reps: +reps }
+    const todaysHistoryEntry = (activeUser as Client).trainingPlan.history.find(
+      (entry) => isToday(entry.date)
+    );
 
-    const todaysHistoryEntry = (activeUser as Client).trainingPlan.history.find(entry => isToday(entry.date))
+    let updatedHistory: HistoryEntry[] = [
+      ...(activeUser as Client).trainingPlan.history,
+    ];
+    console.log("useClientActions ~ updatedHistory", updatedHistory);
 
     if (!todaysHistoryEntry) {
-      const newHistoryEntry: HistoryEntry = { date: today, data: [{ exercise, sets: [newSet] }] }
-      const updatedUser: Client = { ...(activeUser as Client), trainingPlan: { ...(activeUser as Client).trainingPlan, history: [...(activeUser as Client).trainingPlan.history, newHistoryEntry] } }
+      const newHistoryEntry: HistoryEntry = {
+        date: today,
+        data: [],
+      };
 
-      updateUser(updatedUser);
-    } else {
-      console.log("todaysHistoryEntry", todaysHistoryEntry)
-      const updatedTodaysHistoryEntryData: HistoryEntryData[] = []
-      const updatedTodaysHistoryEntry = { ...todaysHistoryEntry, data: updatedTodaysHistoryEntryData }
-      console.log('updatedTodaysHistoryEntry', updatedTodaysHistoryEntry)
-
+      updatedHistory = [...updatedHistory, newHistoryEntry];
     }
+    // At this point, there will always be a an entry for today
 
+    updatedHistory = updatedHistory.map((historyEntry: HistoryEntry) => {
+      if (isToday(historyEntry.date)) {
+        // check if current historyEntry has matching exercise data
+        const historyEntryHasExistingExerciseData = historyEntry.data.find(
+          (historyEntryData: HistoryEntryData) =>
+            historyEntryData.exercise === exercise
+        );
 
+        let updatedHistoryEntry: HistoryEntry = { ...historyEntry };
 
+        // if not, create one and add to current history entry
+        if (!historyEntryHasExistingExerciseData) {
+          const newHistoryEntryData: HistoryEntryData = { exercise, sets: [] };
+          // Add to current HistoryEntry
+          updatedHistoryEntry = {
+            ...updatedHistoryEntry,
+            data: [...updatedHistoryEntry.data, newHistoryEntryData],
+          };
+        }
 
+        //TODO: iterate through data, once at the matching exercise, add to current set
 
+        // AT THIS POINT, THERE WILL ALWAYS BE MATCHING EXERCISE DATA in the current entry
+        // ADD SET TO MATCHING EXERCISE
 
+        // DO NOT CHANGE BELOW
+      }
 
+      // Default return if current iteration is not todays entry
+      return historyEntry;
+    });
 
+    // DO NOT CHANGE ANYHTING BELOW
+
+    const updatedUser: Client = {
+      ...(activeUser as Client),
+      trainingPlan: {
+        ...(activeUser as Client).trainingPlan,
+        history: updatedHistory,
+      },
+    };
+
+    updateUser(updatedUser);
   };
 
   const deleteSetFromLog = (
     historyEntry: HistoryEntry,
     exercise: string,
     setIndex: number
-  ) => { };
+  ) => {};
 
   return {
     fetchTodaysWorkoutEntries,
