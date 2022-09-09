81295090064782e8b78e29a1ce34b02c7dffb458
Karl Cereno <kagc.dev@gmail.com>
2022-09-09 13:15:07 -0400

    implement rough tab design

--- src/pages/ClientDashboard/ClientDashboard.tsx
+++ src/pages/ClientDashboard/ClientDashboard.tsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Container, Tabs, Tab } from "react-bootstrap";
 import { Client } from "../../data/interfaces";
 import HistoryAccordion from "../../components/Client/HistoryAccordion/HistoryAccordion";
 import useClientActions from "../../hooks/useClientActions";
@@ -17,14 +17,27 @@ const ClientDashboard = ({
 
   return (
     <Container className="text-white my-5" style={{ maxWidth: "700px" }}>
-      <h1>Hello {firstName}</h1>
-      <p>This is your program today is</p>
-      <HistoryAccordion
-        program={program}
-        todaysHistoryEntry={todaysHistoryEntry}
-      />
+      <Tabs defaultActiveKey="todaysWorkout" className="mb-3">
+        <Tab eventKey="todaysWorkout" title="Todays Workout">
+          <HistoryAccordion
+            program={program}
+            todaysHistoryEntry={todaysHistoryEntry}
+          />
+        </Tab>
+        <Tab eventKey="profile" title="Previous Workouts">
+          <h1>History</h1>
+        </Tab>
+      </Tabs>
     </Container>
   );
 };
 
 export default ClientDashboard;
+
+{
+  /* <Container className="text-white my-5" style={{ maxWidth: "700px" }}>
+
+<p>This is your program today is</p>
+
+</Container> */
+}
