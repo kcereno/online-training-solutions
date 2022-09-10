c8472a4ffa0e1cfa6e8cf775e327c78807ce072f
Karl Cereno <kagc.dev@gmail.com>
2022-09-10 12:39:54 -0400

    Make HistoryAccorion

---
+++ src/components/Shared/HistoryAccordion/HistoryAccordion.tsx
@@ -0,0 +1,61 @@
+import { Accordion, Card, useAccordionButton } from "react-bootstrap";
+import { HistoryEntry } from "../../../data/interfaces";
+
+interface Props {
+  history: HistoryEntry[];
+}
+
+const HistoryAccordion = ({ history }: Props) => {
+  interface CustomToggleInterface {
+    children: React.ReactNode;
+    eventKey: string;
+  }
+
+  function CustomToggle({ children, eventKey }: CustomToggleInterface) {
+    const decoratedOnClick = useAccordionButton(eventKey);
+
+    return (
+      <p
+        style={{ marginTop: "auto", marginBottom: "auto" }}
+        onClick={decoratedOnClick}
+      >
+        {children}
+      </p>
+    );
+  }
+
+  return (
+    <Accordion defaultActiveKey="0" className="pb-3">
+      {history.map((HistoryEntry: HistoryEntry) => (
+        <Card
+          style={{ background: "#212529" }}
+          key={HistoryEntry.date.toDateString()}
+        >
+          <Card.Header>
+            <CustomToggle eventKey={HistoryEntry.date.toDateString()}>
+              {HistoryEntry.date.toDateString()}
+            </CustomToggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey={HistoryEntry.date.toDateString()}>
+            <Card.Body style={{ background: "black" }}>
+              {HistoryEntry.data.map((data, index) => (
+                <div key={index.toString()}>
+                  <p>{data.exercise}</p>
+                  {data.sets.map((set, index) => (
+                    <ul key={index.toString()}>
+                      <li>{`Set: ${index + 1} Weight: ${set.weight} Reps: ${
+                        set.reps
+                      }`}</li>
+                    </ul>
+                  ))}
+                </div>
+              ))}
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      ))}
+    </Accordion>
+  );
+};
+
+export default HistoryAccordion;
