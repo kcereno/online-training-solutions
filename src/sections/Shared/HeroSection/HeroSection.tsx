a3ebf058006e82603c186342a3f633248b3d527d
Karl Cereno <kagc.dev@gmail.com>
2022-09-16 12:42:37 -0400

    update Hero css

--- src/sections/Shared/HeroSection/HeroSection.tsx
+++ src/sections/Shared/HeroSection/HeroSection.tsx
@@ -1,12 +1,14 @@
 import { Button, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./HeroSection.css";
+import "./HeroSection.scss";
 
 const HeroSection = () => (
   <section id="showcase">
     <Container className="text-white text-center vertical-align">
       <Row>
-        <h1 className="display-2 py-3 ">Where Trainers and Clients Connect</h1>
+        <h1 className="display-2 py-3 hero-text ">
+          Where Trainers and Clients Connect
+        </h1>
         <div className="button-group">
           <Link to="/signin">
             <Button className="mx-2" variant="outline-light" size="lg">
