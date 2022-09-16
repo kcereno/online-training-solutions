e1f0efc775054520d3c9c75026f0aa2b7ec529ca
Karl Cereno <kagc.dev@gmail.com>
2022-09-16 13:01:06 -0400

    Update nav css

--- src/UI/NavBar/NavBar.tsx
+++ src/UI/NavBar/NavBar.tsx
@@ -1,16 +1,16 @@
 import { Container, Navbar } from "react-bootstrap";
 import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useContext } from "react";
 import UserContext from "../../store/User/user-context";
-
 import NavBarToggleMenu from "./NavBarToggleMenu/NavBarToggleMenu";
+import { useContext } from "react";
+import "./Navbar.scss";
 
 const NavBar = () => {
   const { activeUser, logout } = useContext(UserContext);
 
   return (
-    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="md" variant="dark" className="color-nav">
       <Container fluid>
         <Navbar.Brand href="/">
           <FontAwesomeIcon icon={faDumbbell} className="px-2" />
