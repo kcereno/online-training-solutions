1df1f67c1ac773ca3dcae5143bca2ceeb05ac6ae
Karl Cereno <kagc.dev@gmail.com>
2022-09-24 13:26:25 -0400

    Add style sytem to hero section

--- src/sections/Shared/HeroSection/HeroSection.tsx
+++ src/sections/Shared/HeroSection/HeroSection.tsx
@@ -3,19 +3,15 @@ import { Link } from "react-router-dom";
 import "./HeroSection.scss";
 
 const HeroSection = () => (
-  <section id="showcase">
-    <Container className="text-white text-center ">
-      <Row className="mx-auto vertical-center">
-        <h1 className="display-2 py-3 hero-text ">
-          Where Trainers and Clients Connect
-        </h1>
-        <div className="button-group">
-          <Link to="/signin">
-            <Button className="mx-2" variant="outline-light" size="lg">
-              Log In
-            </Button>
-          </Link>
-        </div>
+  <section className="hero-section">
+    <Container>
+      <Row className="center hero-container">
+        <h1 className="hero-heading ">Where Trainers and Clients Connect</h1>
+        <Link to="/signin">
+          <Button className="mx-2" variant="outline-light" size="lg">
+            Log In
+          </Button>
+        </Link>
       </Row>
     </Container>
   </section>
