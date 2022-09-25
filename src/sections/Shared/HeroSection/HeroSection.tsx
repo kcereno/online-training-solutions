3a24190a629bc49b02b6f874bd18f8b7922da561
Karl Cereno <kagc.dev@gmail.com>
2022-09-25 10:44:56 -0400

    Make heading global class

--- src/sections/Shared/HeroSection/HeroSection.tsx
+++ src/sections/Shared/HeroSection/HeroSection.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import "./HeroSection.scss";
 
 const HeroSection = () => (
-  <section className="hero-section">
+  <section className="hero-section ">
     <Container>
-      <Row className="center hero-container">
-        <h1 className="hero-heading ">Where Trainers and Clients Connect</h1>
+      <Row className="center w-75 ">
+        <h1 className="heading ">Where Trainers and Clients Connect</h1>
         <Link to="/signin">
           <Button className="mx-2" variant="outline-light" size="lg">
             Log In
