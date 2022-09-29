db967c73b5b8587672c0bc075724b8b4c3d38866
Karl Cereno <karlcereno@gmail.com>
2022-09-29 02:08:40 +0000

    Make seconday bg color global class

--- src/sections/Shared/TestiomonialsSection/TestiomonialsSection.tsx
+++ src/sections/Shared/TestiomonialsSection/TestiomonialsSection.tsx
@@ -1,17 +1,15 @@
 import { Container } from "react-bootstrap";
-import "./TestiomonialsSection.scss";
 import TestimonialCard from "./TestomonialCard/TestimonialCard";
-
 import image1 from "./../../../assets/images/testimonial-faces/image-lorem-face-3134.jpeg";
 import image2 from "../../../assets//images//testimonial-faces/image-lorem-face-34343134.jpeg";
 import image3 from "../../../assets/images/testimonial-faces/image-lorem-face-5178.jpeg";
+import "./TestiomonialsSection.scss";
 
 const TestiomonialsSection = () => (
-  <section className="section testimonials">
-    <Container fluid className="carousel text-center text-white">
+  <section className="section flex-wrap secondary-bg-color">
+    <Container fluid className="text-center text-white">
       <h2 className="subheading">Testimonials</h2>
-
-      <div className="testimonials d-flex justify-content-center flex-wrap">
+      <div className="d-flex justify-content-center ">
         <TestimonialCard name="Hector Fabuloso" photo={image1} />
         <TestimonialCard name="Patrick Bueno" photo={image2} />
         <TestimonialCard name="Ricardo Mantalban" photo={image3} />
