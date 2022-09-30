79b29beb6d70dd54f8c638c145e663f69f288182
Karl Cereno <karlcereno@gmail.com>
2022-09-30 01:02:42 +0000

    Add padding to navbar

--- src/UI/NavBar/NavBar.tsx
+++ src/UI/NavBar/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
 
   return (
     <Navbar collapseOnSelect expand="md" variant="dark" className="navbar py-1">
-      <Container fluid>
+      <Container fluid className="py-2">
         <Navbar.Brand href="/" className="body-text">
           <FontAwesomeIcon icon={faDumbbell} className="px-2" />
           Online Training Solutions
